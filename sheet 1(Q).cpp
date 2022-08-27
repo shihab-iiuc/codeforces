bb819cb4d9ab22de533339fc6c8a0b90f12203a8
shihab-iiuc <109750541+shihab-iiuc@users.noreply.github.com>
2022-08-27 16:01:20 +0600

    Create sheet 1(Q).cpp

---
+++ sheet 1(Q).cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ 
+   double a,b;
+ 
+  cin>>a>>b;
+ 
+  if(a==0 && b==0)
+    cout<<"Origem";
+  else if(a>0 && b>0)
+    cout<<"Q1";
+  else if(a<0 && b>0)
+    cout<<"Q2";
+  else if(a<0 && b<0)
+    cout<<"Q3";
+ else if(a>0 && b<0)
+    cout<<"Q4";
+ else if((a==0 && b>0)||(a==0 && b<0))
+    cout<<"Eixo Y";
+ else if((a>0 && b==0)||(a<0 && b==0))
+    cout<<"Eixo X";
+ 
+return 0;
+}
+ 
