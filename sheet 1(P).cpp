0cc683e01a5631d2b06b41c0fc86a91211194344
shihab-iiuc <109750541+shihab-iiuc@users.noreply.github.com>
2022-08-27 15:59:12 +0600

    Create sheet 1(P).cpp

---
+++ sheet 1(P).cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ 
+   long long a;
+ 
+  cin>>a;
+  a=a/1000;
+  if(a%2==0)
+    cout<<"EVEN";
+  else
+    cout<<"ODD";
+ 
+return 0;
+}
+ 
