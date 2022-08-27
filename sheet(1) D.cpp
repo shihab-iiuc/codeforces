9c13e336db69df6878540fbba655b7b0eee59f87
shihab-iiuc <109750541+shihab-iiuc@users.noreply.github.com>
2022-08-27 14:20:00 +0600

    Create sheet(1) D.cpp

---
+++ sheet(1) D.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long a,b,c,d,e;
+    cin>>a>>b>>c>>d;
+    e=(a*b)-(c*d);
+    cout<<"Difference"<<" = "<<e <<endl;
+ 
+    return 0;
+ 
+}
