4f109484d945bf14755a747258b72e3a804b045b
shihab-iiuc <109750541+shihab-iiuc@users.noreply.github.com>
2022-08-27 22:02:05 +0600

    Create A.cpp

---
+++ sheet 2/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+      int i,N;
+      cin>>N;
+      for(i=1;i<=N;i++)
+        {
+          cout<<i<<endl;
+        }
+  return 0;
+}
+ 
