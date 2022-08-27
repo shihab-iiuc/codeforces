3c7aac68c594b36ff8a9763eb138cfd91d47f411
shihab-iiuc <109750541+shihab-iiuc@users.noreply.github.com>
2022-08-27 16:12:58 +0600

    Create sheet 1(V).cpp

---
+++ sheet 1(V).cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    char s;
+    cin>>a>>s>>b;
+    if(a>b && s=='>')
+     cout<<"Right";
+    else if(a<b && s=='<')
+      cout<<"Right";
+    else if(a==b && s=='=')
+      cout<<"Right";
+    else
+        cout<<"Wrong";
+ return 0;
+}
