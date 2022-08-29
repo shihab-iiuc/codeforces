635f3d22e61bead259f85afb1f3bc7c6952e82da
shihab-iiuc <109750541+shihab-iiuc@users.noreply.github.com>
2022-08-29 20:00:51 +0600

    Create D.cpp

---
+++ sheet 2/D.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int N,i;
+ 
+    for(i=1;;i++)
+    {
+     cin>>N;
+     if(N==1999)
+     {
+         cout<<"Correct"<<endl;
+         break;
+     }
+   else if(N!=1999)
+     {
+        cout<<"Wrong"<<endl;
+     }
+ 
+    }
+ 
+ 
+return 0;
+}
