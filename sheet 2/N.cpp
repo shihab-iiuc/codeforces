34a9bd637b278b2f741e5cfbf89a69eeba4674dd
shihab-iiuc <109750541+shihab-iiuc@users.noreply.github.com>
2022-08-29 20:22:45 +0600

    Create N.cpp

---
+++ sheet 2/N.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+     char s;
+     int i,j,N,e,k;
+     cin>>s;
+     cin>>N;
+     for(i=1;i<=N;i++)
+      {
+          cin>>e;
+         for(j=1;j<=e;j++)
+           {
+               cout<<s;
+           }
+           cout<<endl;
+      }
+ 
+    return 0;
+}
+Judged Example Tests
