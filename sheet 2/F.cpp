70b74c67def5828496218bbc3ec2cfdd33725501
shihab-iiuc <109750541+shihab-iiuc@users.noreply.github.com>
2022-08-29 20:04:10 +0600

    Create F.cpp

---
+++ sheet 2/F.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int N,i,multi;
+    cin>>N;
+    for(i=1;i<=12;i++)
+    {
+        multi=N*i;
+        cout<<N<<" * "<<i<<" = "<<multi<<endl;
+ 
+    }
+ 
+ 
+return 0;
+}
