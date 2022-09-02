e6decfad5a7a83a593dc29a096b656f9fcb82081
shihab-iiuc <109750541+shihab-iiuc@users.noreply.github.com>
2022-09-02 10:56:25 +0600

    Create A.cpp

---
+++ fuction sheet/A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int add(int a,int b)
+{
+    return a+b;
+}
+int main()
+{
+    int d,e;
+    cin>>d>>e;
+    int f=add(d,e);
+    cout<<f;
+ 
+    return 0;
+}
