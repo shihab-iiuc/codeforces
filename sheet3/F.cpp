6f257449caa4b1d5ce476118b9ee028467c43e30
shihab-iiuc <109750541+shihab-iiuc@users.noreply.github.com>
2022-10-01 19:46:22 +0600

    Create F.cpp

---
+++ sheet3/F.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ long long int i,N;
+    cin>>N;
+    long long int A[N],x;
+    for(i=1;i<=N;i++)
+      {
+          cin>>A[i];
+          
+    
+      }
+      for(i=N;i>=1;i--)
+       {
+           cout<<A[i]<<" ";
+       }
+      
+      return 0;
+}
